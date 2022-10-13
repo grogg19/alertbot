371931377c3ff2722d294b7b854faf50ee79e1e0
grogg19 <differ@list.ru>
2022-10-14 01:48:09 +0300

    feat(config): добавлен параметр bot_username

--- config/telegram.php
+++ config/telegram.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    'host'  => env('APP_URL', 'https://api.telegram.org'),
-    'token' => env('TELEGRAM_TOKEN')
+    'host'         => env('APP_URL', 'https://api.telegram.org'),
+    'token'        => env('TELEGRAM_TOKEN'),
+    'bot_username' => env('TELEGRAM_BOT_USERNAME'),
 ];
