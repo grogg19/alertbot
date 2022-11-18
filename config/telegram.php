6d59952d4dc781db07fe39caa5dabaef2031060f
grogg19 <differ@list.ru>
2022-11-18 18:11:39 +0300

    fix(config): в конфиг добавлен новый параметр chat_id_default

--- config/telegram.php
+++ config/telegram.php
@@ -1,7 +1,8 @@
 <?php
 
 return [
-    'host'         => env('APP_URL', 'https://api.telegram.org'),
-    'token'        => env('TELEGRAM_TOKEN'),
-    'bot_username' => env('TELEGRAM_BOT_USERNAME'),
+    'host'            => env('APP_URL', 'https://api.telegram.org'),
+    'token'           => env('TELEGRAM_TOKEN'),
+    'bot_username'    => env('TELEGRAM_BOT_USERNAME'),
+    'chat_id_default' => env('TELEGRAM_CHAT_ID_DEFAULT'),
 ];
